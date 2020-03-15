f065538a495e491bca7db042f5c335ee29235078
zepcp <vieirapereira@protonmail.com>
2020-03-15 20:15:51 +0000

    new approach

--- schema.sql
+++ schema.sql
@@ -47,6 +47,7 @@ INSERT INTO user_symptoms(symptoms) values('with symptoms');
 CREATE TABLE IF NOT EXISTS users (
     id varchar(16) primary key,
     hash bytea,
+    facebook_id bytea,
     year integer,
     postalcode varchar(8),
     ip varchar(20),
@@ -57,6 +58,7 @@ CREATE TABLE IF NOT EXISTS users (
     unix_ts int default extract(epoch from now())
 );
 CREATE UNIQUE INDEX IF NOT EXISTS users_id0 ON users (id);
+CREATE INDEX IF NOT EXISTS users_id1 ON users (facebook_id);
 --DROP TABLE IF EXISTS users CASCADE;
 
 --Table History With The Whole User Health History:
@@ -74,34 +76,21 @@ CREATE TABLE IF NOT EXISTS history (
 CREATE INDEX IF NOT EXISTS history_id0 ON history (user_id, timestamp);
 --DROP TABLE IF EXISTS history CASCADE;
 
---Table Network With The User Network Info:
+--Table Network With The Whole Users Network/Connections:
 CREATE TABLE IF NOT EXISTS network (
     id serial,
     user_id varchar(16),
-    facebook_id varchar(16),
-    name varchar(100),
-    email varchar(100),
-    url varchar(500),
-    timestamp timestamp without time zone default (now() at time zone 'utc'),
-    unix_ts int default extract(epoch from now())
-);
-CREATE INDEX IF NOT EXISTS network_id0 ON network (user_id);
-CREATE INDEX IF NOT EXISTS network_id1 ON network (facebook_id);
---DROP TABLE IF EXISTS network CASCADE;
-
---Table Network With The Whole Users Network/Connections:
-CREATE TABLE IF NOT EXISTS user_network (
-    id serial,
-    facebook_id varchar(16),
     met_with varchar(16),
+    facebook_id bytea,
     latitude numeric,
     longitude numeric,
     info varchar(500),
     timestamp timestamp without time zone default (now() at time zone 'utc'),
     unix_ts int default extract(epoch from now())
 );
-CREATE INDEX IF NOT EXISTS user_network_id0 ON user_network (facebook_id);
-CREATE INDEX IF NOT EXISTS user_network_id1 ON user_network (met_with);
+CREATE INDEX IF NOT EXISTS network_id0 ON network (user_id);
+CREATE INDEX IF NOT EXISTS network_id1 ON network (met_with);
+CREATE INDEX IF NOT EXISTS network_id2 ON network (facebook_id);
 --DROP TABLE IF EXISTS network CASCADE;
 
 --View To Easily Get Latest User Health Status For Each User:
@@ -118,6 +107,19 @@ BEGIN
 END$$;
 --DROP VIEW latest_status;
 
+--View To Easily Get Latest Network Health Status For Each User:
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'network_status') THEN
+        CREATE VIEW network_status AS
+            SELECT b.user_id, b.met_with, c.status, c.symptoms, c.timestamp
+            FROM users a
+            INNER JOIN network b ON a.id = b.user_id
+            INNER JOIN latest_status c ON b.met_with = c.user_id;
+    END IF;
+END$$;
+--DROP VIEW network_status;
+
 --Dummy Data For Test Proposes
 DELETE FROM users where id in ('1','2','3');
 DELETE FROM history where user_id in ('1','2','3');
@@ -125,24 +127,24 @@ DELETE FROM network where user_id in ('1','2','3');
 INSERT INTO users(id, hash, year, postalcode, ip, info) values('1', '1', 1, '1000-100', null, null);
 INSERT INTO users(id, hash, year, postalcode, ip, info) values('2', '2', 1, '1000-100', null, null);
 INSERT INTO users(id, hash, year, postalcode, ip, info) values('3', '3', 1, '1000-100', null, null);
-INSERT INTO network(user_id, facebook_id, name, email, url) values('1', '1', 'name1', 'email1@email.com', 'http://url1.com');
-INSERT INTO network(user_id, facebook_id, name, email, url) values('2', '2', 'name2', 'email2@email.com', 'http://url2.com');
-INSERT INTO network(user_id, facebook_id, name, email, url) values('3', '3', 'name3', 'email3@email.com', 'http://url3.com');
 INSERT INTO history(user_id, status, symptoms) values('1', 0, 0);
 INSERT INTO history(user_id, status, symptoms) values('1', 3, 1);
 INSERT INTO history(user_id, status, symptoms) values('1', 4, 0);
 INSERT INTO history(user_id, status, symptoms) values('2', 0, 0);
 INSERT INTO history(user_id, status, symptoms) values('2', 3, 1);
 INSERT INTO history(user_id, status, symptoms) values('3', 0, 0);
-INSERT INTO user_network(facebook_id, met_with) values('1', '2');
-INSERT INTO user_network(facebook_id, met_with) values('1', '3');
-INSERT INTO user_network(facebook_id, met_with) values('2', '1');
-INSERT INTO user_network(facebook_id, met_with) values('2', '3');
-INSERT INTO user_network(facebook_id, met_with) values('3', '1');
-INSERT INTO user_network(facebook_id, met_with) values('3', '2');
+INSERT INTO network(user_id, met_with) values('1', '2');
+INSERT INTO network(user_id, met_with) values('1', '3');
+INSERT INTO network(user_id, met_with) values('2', '1');
+INSERT INTO network(user_id, met_with) values('2', '3');
+INSERT INTO network(user_id, met_with) values('3', '1');
+INSERT INTO network(user_id, met_with) values('3', '2');
 
 --Get Current Status Count:
 SELECT status, count(1) FROM latest_status GROUP BY status;
+
+--Get Current Network Status Count For User '1':
+SELECT status, count(1) FROM network_status WHERE user_id='1' GROUP BY status;
 
 --Dummy Table With Column Type Examples
 CREATE TABLE IF NOT EXISTS example (
