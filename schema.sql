796584772dbca74b2285aa3e3e9c0f1448a3ed32
zepcp <vieirapereira@protonmail.com>
2020-03-15 15:02:57 +0000

    network status

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@ ALTER TYPE statustype ADD VALUE 'quarentine';
 --DROP TYPE IF EXISTS statustype CASCADE;
 
 CREATE TABLE users (
-    id varchar(16),
+    id varchar(16) primary key,
     hash bytea,
     age integer,
     city varchar(20),
@@ -20,7 +20,7 @@ CREATE TABLE history (
     symptoms bool,
     timestamp int default extract(epoch from now())
 );
-CREATE INDEX history_id0 ON history (user_id);
+CREATE INDEX history_id0 ON history (user_id, timestamp);
 --DROP TABLE IF EXISTS history CASCADE;
 
 CREATE TABLE network (
@@ -39,12 +39,12 @@ CREATE VIEW latest_status AS
           AND a.timestamp < b.timestamp);
 
 CREATE VIEW network_status AS
-    SELECT a.user_id, a.status, a.symptoms, a.timestamp
-    FROM history a WHERE NOT EXISTS (
-        SELECT 1 FROM history b
-        WHERE a.user_id = b.user_id
-          AND a.timestamp < b.timestamp);
+    SELECT b.user_id, b.met_with, c.status, c.symptoms, c.timestamp
+    FROM users a
+    INNER JOIN network b ON a.id = b.user_id
+    INNER JOIN latest_status c ON b.met_with = c.user_id;
 
+--DUMMY DATA
 INSERT INTO users(id, hash, age, city, ip, info) values('1', '1', 1, 'Lisboa', null, null);
 INSERT INTO users(id, hash, age, city, ip, info) values('2', '2', 1, 'Lisboa', null, null);
 INSERT INTO users(id, hash, age, city, ip, info) values('3', '3', 1, 'Lisboa', null, null);
@@ -54,6 +54,16 @@ INSERT INTO history(user_id, status, symptoms) values('1', 'recovered', False);
 INSERT INTO history(user_id, status, symptoms) values('2', 'normal', False);
 INSERT INTO history(user_id, status, symptoms) values('2', 'infected', True);
 INSERT INTO history(user_id, status, symptoms) values('3', 'normal', False);
+INSERT INTO network(user_id, met_with) values('1', '2');
+INSERT INTO network(user_id, met_with) values('1', '3');
+INSERT INTO network(user_id, met_with) values('2', '1');
+INSERT INTO network(user_id, met_with) values('2', '3');
+INSERT INTO network(user_id, met_with) values('3', '1');
+INSERT INTO network(user_id, met_with) values('3', '2');
+
+--QUERY EXAMPLES
+SELECT status, count(1) FROM latest_status GROUP BY status;
+SELECT status, count(1) FROM network_status WHERE user_id='1' GROUP BY status;
 
 --CREATE TABLE examples (
 --    user_id varchar(16) references users(id),
