8323a5baf5ea3488c6207dae23d8f8b4150f6eb1
Ricardo <ricardo.fernandes@bitmaker-software.com>
2020-03-17 21:02:42 +0000

    Retrieving cases for a postal code and fixing some model issues (#51)
    * Rerieving cases for a postal code and fixing some model issues related with associations
    
    * Adding endpoints for getting status and confinement states by postal code plus storing phone and email info.
    
    * bugfix on updating info
    
    Co-authored-by: Ricardo Fernandes <itsector@admins-MBP.lan>

--- plugins/models.js
+++ plugins/models.js
@@ -9,18 +9,20 @@ module.exports = fp(async (fastify, opts) => {
         const UserSymptom = fastify.sequelize.import('../db/models/user_symptoms.js');
         const ConfinementState = fastify.sequelize.import('../db/models/confinement_states.js');
         const Condition = fastify.sequelize.import('../db/models/user_status.js');
+        const StatusByPostalCode = fastify.sequelize.import('../db/models/status_by_postalcode.js');
+        const ConfinementStateByPostalCode = fastify.sequelize.import('../db/models/confinement_state_by_postalcode.js');
 
         //relationships       
-        Case.belongsTo(Users, { foreignKey: 'id' });
+        Case.belongsTo(Users, { foreignKey: 'user_id' });
         Users.hasMany(Case, { foreignKey: 'user_id' });
         
         Users.hasMany(Network, { foreignKey: 'user_id' });
-        Network.belongsTo(Users, { foreignKey: 'id' });
+        Network.belongsTo(Users, { foreignKey: 'user_id' });
 
         Symptom.hasMany(UserSymptom, { foreignKey: 'symptom_id' });
         Case.hasMany(UserSymptom, { foreignKey: 'history_id' });
-        UserSymptom.belongsTo(Case, { foreignKey: 'id' });
+        UserSymptom.belongsTo(Case, { foreignKey: 'history_id' });
 
-        return { Case, Network, Users, Symptom, ConfinementState, Condition }
+        return { Case, Network, Users, Symptom, ConfinementState, Condition, StatusByPostalCode, ConfinementStateByPostalCode }
     })
 })
