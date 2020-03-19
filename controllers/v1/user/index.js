1fc82e4414091844d982dd99a395d580634dc205
João Duarte <deluxor@users.noreply.github.com>
2020-03-19 22:11:13 +0000

    added dynamic redirect url after login from FB, removed unused output.pdf (#71)

--- controllers/v1/user/index.js
+++ controllers/v1/user/index.js
@@ -42,19 +42,17 @@ module.exports = async (fastify, opts) => {
   }, async (request, reply) => {
     try {
       const { year, postalCode, geo, phone, email, name, patientToken, showOnboarding } = request.body;
-      console.log(phone);
-      console.log(email);
       const user = await fastify.models().Users.findOne({
         where: { id: request.user.payload.id },
       });
-      
+
       // Fill info
-      var info = JSON.parse(user.info != null ? user.info : '{}');     
+      var info = JSON.parse(user.info != null ? user.info : '{}');
       info.version = 1
       info.name = name !== undefined ? name : info.name;
       info.phone = phone !== undefined ? phone : info.phone;
       info.email = email !== undefined ? email : info.email;
- 
+
       await fastify.models().Users.update({ year, postalcode: postalCode, latitude: geo.lat, longitude: geo.lon, info: JSON.stringify(info), unix_ts: Date.now(), patient_token: patientToken, show_onboarding: showOnboarding }, { where: { id: request.user.payload.id }, fields: ['year', 'postalcode', 'latitude', 'longitude', 'unix_ts', 'info', 'patient_token', 'show_onboarding'] })
       reply.send({ status: 'ok' });
     } catch (error) {
