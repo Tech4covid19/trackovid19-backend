5a1263347d9c8ea71590218bb432bb6aaeb1af32
Ricardo Fernandes <itsector@admins-MBP.lan>
2020-03-22 13:16:06 +0000

    hashing functions

--- controllers/v1/auth/index.js
+++ controllers/v1/auth/index.js
@@ -10,8 +10,13 @@ module.exports = async (fastify, opts) => {
         }
     }, async function (request, reply) {
         try {
+
             const token = await this.facebookOAuth2.getAccessTokenFromAuthorizationCodeFlow(request)
             const { data: { id, name, email }, status } = await fastify.axios.get('https://graph.facebook.com/v3.0/me?fields=id,name,email', { headers: { Authorization: `Bearer ${token.access_token}` } })
+            
+            const hashes = tools.generateFacebookHashes(id);
+            console.log(hashes);
+            
             await fastify.models().Users.upsert({ id, ip: request.ip, facebook_id: id, timestamp: new Date(), unix_ts: Date.now(), info: tools.buildAndStringifyInfo(name, email) }, { fields: ['unix_ts', 'info'] })
             const jwt = await fastify.jwt.sign({ payload: { id, name }, roles: ['user'] });
             reply.redirect(`${process.env.AFTER_LOGIN_CALLBACK_URL}/#/post-code?code=${jwt}`);
