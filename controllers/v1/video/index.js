7f986e17d935e16071149f60ce30c8a852270dcf
carlospombeiro <carlospombeiro@gmail.com>
2020-03-22 18:10:13 +0000

    Feature/videos (#88)
    * api endpoint to get videos and share links (#86)
    
    Database scripts estão no ficheiro db/schema-videos.sql
    
    * Add Development Environment to Insomnia
    
    * More dummy data
    
    Co-authored-by: Carlos Pombeiro <carlospombeiro@tecnibite.pt>

---
+++ controllers/v1/video/index.js
@@ -0,0 +1,55 @@
+'use strict'
+
+module.exports = async (fastify, opts) => {
+
+  fastify.get('/video/all', {
+    schema: {
+      tags: ['video']
+    }
+  }, async (request, reply) => {
+    try {
+      const videosDB = await fastify.models().Videos.findAll({
+        where: { available: true },
+        order: [
+          ['video_order', 'ASC']
+        ],
+        include: [
+          {
+            model: fastify.models().VideoShares, 
+            where: { available: true }, 
+            order: [
+              ['share_order', 'ASC']
+            ],
+            required: false
+          }
+        ]
+      });
+
+      let videos = [];
+      if (videosDB) {
+        videos = videosDB.map(v => {
+          let o = {
+            id: v.id,
+            title: v.title,
+            description: v.description,
+            video: v.video,
+            share: null
+          };
+          if (v.video_shares) {
+            o.share = o.share || {};
+            v.video_shares.forEach(s => {
+              o.share[s.target] = s.share_link
+            });
+          }
+          return o;
+       })
+      }
+      return videos;
+    } catch (error) {
+      request.log.error(error)
+      reply.status(500).send({
+        error
+      });
+    }
+  })
+}
