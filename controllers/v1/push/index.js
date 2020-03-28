545fac96c0ab6acff914db89146c527dbb406f55
carlospombeiro <carlospombeiro@gmail.com>
2020-03-28 21:12:30 +0000

    Feature/web push notifications improvements (#130)
    * Web push notifications to Chrome, Firefox & Edge
    
    * Add /public/tests/webpush.html page for testing
    * Add fastify-static for static content
    * API methods: /push/web/vapidPublicKey, /push/web/register, /push/web
    
    * Interface with DB
    
    * Insomnia environments Dev, Staging, PROD
    
    * Refactoring
    
    * Send notification with an icon
    
    * Include icon url in notification payload
    
    * Include badge and image url in notification payload, very limited support by devices
    
    * Improve test page /public/tests/webpush.html
    
    * Sanitize error response in get videos

--- controllers/v1/push/index.js
+++ controllers/v1/push/index.js
@@ -1,8 +1,6 @@
 'use strict'
 
-const webpush = require('web-push');
-
-let subscription;
+const webPush = require('../../../services/web-push-service');
 
 module.exports = async (fastify, opts) => {
 
@@ -25,7 +23,7 @@ module.exports = async (fastify, opts) => {
   fastify.post('/push/web/register', {
     preValidation: [fastify.authenticate]
   }, async (request, reply) => {
-    subscription = request.body.subscription;
+    const subscription = request.body.subscription;
 
     if (!subscription || !subscription.endpoint || !subscription.keys) {
       reply.status(500).send({
@@ -34,10 +32,15 @@ module.exports = async (fastify, opts) => {
       return;
     }
 
-    let pushSubscription;
-
     try {
-      pushSubscription = await getUserPushSubscription(request.user.payload.id, subscription.endpoint);
+      // get user subscription
+      let pushSubscription = await fastify.models().PushSubscriptions.findOne({
+        where: { 
+          user_id: request.user.payload.id, 
+          push_type: 'web-push', 
+          endpoint: subscription.endpoint 
+        }
+      });
 
       if (!pushSubscription) {
         pushSubscription = await fastify.models().PushSubscriptions.create({ 
@@ -54,9 +57,7 @@ module.exports = async (fastify, opts) => {
       reply.status(500).send(sanitize_log(error, 'Could not register subscription'));
     }
 
-    subscription = request.body.subscription;
-    console.log(subscription);
-    return {};
+    reply.send({ status: 'ok' });
   });
 
   fastify.post('/push/web', {
@@ -70,27 +71,43 @@ module.exports = async (fastify, opts) => {
       return;
     }
 
-    let sends = [];
-    let pushSubscriptions;
     try {
-      pushSubscriptions = await fastify.models().PushSubscriptions.findAll({
+      const pushSubscriptions = await fastify.models().PushSubscriptions.findAll({
         where: { 
           user_id: request.body.user_id, 
           push_type: 'web-push'
         }
       });
 
-      if (pushSubscriptions) {
+      if (pushSubscriptions && pushSubscriptions.length > 0) {
+        let sends = [];
+
         for (const pushSub of pushSubscriptions) {
           const subscription = {
             endpoint: pushSub.endpoint,
             keys: JSON.parse(pushSub.keys)
           };
           try {
-            await sendNotification(subscription, request.body.title, request.body.body);
+            const notification = {
+              title: request.body.title,
+              body: request.body.body,
+              icon: request.body.icon,
+              badge: request.body.badge,
+              image: request.body.image
+            }
+            
+          await webPush.sendNotification(subscription, notification);
+            console.log('Web Push Application Server - Notification sent to ' + subscription.endpoint);
             sends.push(subscription.endpoint);
           } catch (error) {
+            console.log('ERROR in sending Notification to ' + subscription.endpoint);
+            console.log(error);
+            request.log.error(error)
             // to update or not update the send_error_count in push_subscriptions table ?!
+            reply.status(500).send({
+              error: 'Error in sending Notification'
+            });
+            return;
           }
         }
         return {
@@ -100,9 +117,7 @@ module.exports = async (fastify, opts) => {
           }
         }
       } else {
-        reply.status(200).send({
-          message: 'No subscriptions for user'
-        });
+        reply.status(404).send({ error: "No subscriptions for user" });
       }
     } catch (error) {
       request.log.error(error)
@@ -111,48 +126,4 @@ module.exports = async (fastify, opts) => {
 
   });
 
-  function sendNotification(subscription, title, body) {
-    return new Promise(function(resolve, reject) {
-      try {
-        const payload = JSON.stringify({
-          title: title,
-          body: body
-        });
-  
-        webpush.setGCMAPIKey(process.env.GCM_API_KEY);
-        //console.log(webpush.generateVAPIDKeys());
-        webpush.setVapidDetails(
-          process.env.VAPID_SUBJECT,
-          process.env.VAPID_PUBLIC_KEY,
-          process.env.VAPID_PRIVATE_KEY
-        );
-  
-        // const options = {
-        //   TTL: 0 // Time to live 
-        // };
-  
-        webpush.sendNotification(subscription, payload) //, options)
-        .then(function() {
-          console.log('Web Push Application Server - Notification sent to ' + subscription.endpoint);
-          resolve();
-        }).catch(function(e) {
-          console.log('ERROR in sending Notification to ' + subscription.endpoint);
-          // delete subscriptions[subscription.endpoint];
-          reject(e);
-        });
-      } catch(e) {
-        reject(e);
-      }
-    });
-  }
-
-  async function getUserPushSubscription(userId, endpoint) {
-    return await fastify.models().PushSubscriptions.findOne({
-      where: { 
-        user_id: userId, 
-        push_type: 'web-push', 
-        endpoint: endpoint 
-      }
-    });
-  }
 }
