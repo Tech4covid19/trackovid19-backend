fff9472b2a233c6039a9c44c6036344dffc89272
Miguel Pinto <miguel.pinto@lookatitude.com>
2020-03-31 03:09:58 +0100

    Added service to generate image on the service folder. Updated svg source to match ids of the text fields. Added service documentation on the comments of the public function.

--- services/image-generator.js
+++ services/image-generator.js
@@ -42,6 +42,7 @@ function generateImage (svg, data) {
         console.log(e)
     }
 
+
 }
 
 /**
@@ -79,9 +80,11 @@ function generateImage (svg, data) {
  * generateDashboard(data);
  */
 async function generateDashboard (data) {
+
     const img = './resources/Share_image_dashboard.svg'
 
     const svg = fs.readFileSync(img).toString()
+
 
     // Add the required fields for data validation
     const fields = {
