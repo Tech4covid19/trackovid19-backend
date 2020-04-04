71f7ce427a56254c08ba7553bb650615c47940d8
Miguel Pinto <miguel.pinto@lookatitude.com>
2020-04-04 14:54:48 +0100

    Added table for share images

---
+++ db/models/share_images_by_postalcode.js
@@ -0,0 +1,23 @@
+/* jshint indent: 1 */
+
+module.exports = function (sequelize, DataTypes) {
+    return sequelize.define('share_images_by_postalcode', {
+        postalcode: {
+            field: 'postalcode1',
+            type: DataTypes.STRING,
+            allowNull: true,
+            primaryKey: true,
+        },
+        image_hash: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        image_url: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+    }, {
+        tableName: 'share_images_by_postalcode',
+        timestamps: false,
+    })
+}
